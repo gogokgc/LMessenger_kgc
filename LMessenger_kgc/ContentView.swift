f78225a6e3d3ac25a9376744d18f2513a7fd5cd4
gogokgc <gogokgc@gmail.com>
2024-06-02 14:13:34 +0900

    Initial Commit

---
+++ LMessenger_kgc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LMessenger_kgc
+//
+//  Created by KYUCHEOL KIM on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LMessenger_kgc/LMessenger_kgcApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMessenger_kgcApp.swift
+//  LMessenger_kgc
+//
+//  Created by KYUCHEOL KIM on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LMessenger_kgcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
