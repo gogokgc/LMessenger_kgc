2eac8da47d6602cb2b2c0bfd8a8c8ff7a929176d
gogokgc <gogokgc@gmail.com>
2024-06-06 17:15:51 +0900

    LoginIntroView 추가

--- LMessenger_kgc/Login/LoginIntroView.swift
+++ LMessenger_kgc/Login/LoginIntroView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct LoginIntroView: View {
+    @State private var isPresentedLoginView: Bool = false
+    
     var body: some View {
-        Text("LoginIntroView")
+        NavigationStack {
+            VStack(spacing: 20) {
+                Spacer()
+                
+                Text("환영합니다.")
+                    .font(.system(size: 26, weight: .bold))
+                    .foregroundColor(.bkText)
+                Text("무료 메시지와 영상통화, 음성통화를 부담없이 즐겨보세요!")
+                    .font(.system(size: 12))
+                    .foregroundColor(.greyDeep)
+                
+                Spacer()
+                
+                Button {
+                    //TODO: 로그인 액션
+                    isPresentedLoginView.toggle()
+                } label: {
+                    Text("로그인")
+                        .font(.system(size: 14))
+                        .foregroundColor(.lineApp)
+                        .frame(maxWidth: .infinity, maxHeight: 40)
+                }
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.lineApp, lineWidth: 0.8)
+                }
+                .padding(.horizontal, 15)
+            }
+            .navigationDestination(isPresented: $isPresentedLoginView) {
+                LoginView()
+            }
+        }
     }
 }
 
---
+++ LMessenger_kgc/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  LMessenger_kgc
+//
+//  Created by KYUCHEOL KIM on 6/6/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("LoginView")
+    }
+}
+
+#Preview {
+    LoginView()
+}
